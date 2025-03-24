3e8c7d4526f84ec79fb350e2174a6c3fbac10a29
Vinicius Lucas <vinicius.lucas@gmail.com>
2025-03-24 14:08:43 -0300

    Configuração do Vite para usar 0.0.0.0 como host

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.1.57',
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
     cors: true
   },
   preview: {
-    host: '192.168.1.57',
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
     cors: true
